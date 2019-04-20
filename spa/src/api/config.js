5a45507d471cf068bd28cbebb88bc6e948dad44b
Naylin Medina <mejiasnam@gmail.com>
2019-04-20 15:48:44 -0400

    add config file
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

---
+++ spa/src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+    appname: 'spa',
+    apiKey: '0c730f8dbf66822d611fb3a8be135a21',
+    secret: 'f43f551b8f0dde58052e0954ea86b00e',
+    registeredTo: 'namm_'
+}
+
+
+// Application name	spa
+// API key	0c730f8dbf66822d611fb3a8be135a21
+// Shared secret	f43f551b8f0dde58052e0954ea86b00e
+// Registered to	namm_
