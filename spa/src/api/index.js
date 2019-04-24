b6b83735fc81afa4bc9b04af70c52a2d9ed8b489
Naylin Medina <mejiasnam@gmail.com>
2019-04-23 21:28:11 -0400

    add api
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

---
+++ spa/src/api/index.js
@@ -0,0 +1,10 @@
+import config from './config'
+
+const { apiKey } = config // const API_KEY = config.apiKey
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=australia&api_key=${apiKey}&format=json`
+
+export default function getArtists () {
+    return fetch(URL)
+        .then(res => res.json())
+        .then(json => json.topartists.artist)
+}
